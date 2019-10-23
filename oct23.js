65a9ef1b37b324bd66f7656fad6cb80bd7171b12
Anthony Betances <tonybetancesdev@gmail.com>
2019-10-23 15:39:04 -0400

    CodeWars / Grasshopper - Function Syntax Debugging / JS / https://www.codewars.com/kata/56dae9dc54c0acd29d00109a

---
+++ oct23.js
@@ -0,0 +1,15 @@
+//Grasshopper - Function syntax debugging
+
+/*A student was working on a function and made some syntax mistakes while coding.
+Help them find their mistakes and fix them.*/
+
+//PROBLEM
+// function main [verb, noun]
+//   return verb + noun
+// }
+
+//SOLUTION
+
+function main (verb, noun){
+  return verb + noun
+}
