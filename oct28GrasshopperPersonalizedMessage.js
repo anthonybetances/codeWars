56afd983b55aa0fb004277fe43b2329ce0694f21
Anthony Betances <tonybetancesdev@gmail.com>
2019-10-28 23:53:43 -0400

    CodeWars / Grasshopper - A Personalized Message / JS / https://www.codewars.com/kata/5772da22b89313a4d50012f7

---
+++ oct28GrasshopperPersonalizedMessage.js
@@ -0,0 +1,23 @@
+//Grasshopper - Personalized Message
+
+/* Instructions
+Personalized greeting
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+
+case	                 return
+name equals owner	     'Hello boss'
+otherwise	'Hello guest' */
+
+
+//ANSWER
+
+function greet (name, owner) {
+  // Add code here
+  if (name === owner){
+  return 'Hello boss'
+}else{
+  return 'Hello guest'
+  }
+}
