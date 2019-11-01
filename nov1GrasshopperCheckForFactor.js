36c3cb8be9a29f3c4929ba75b89fa42c49d9c5fb
Betances <tonybetancesdev@gmail.com>
2019-11-01 18:46:59 -0400

    CodeWars / Grasshopper - Check for Factor / JS / https://www.codewars.com/kata/55cbc3586671f6aa070000fb

---
+++ nov1GrasshopperCheckForFactor.js
@@ -0,0 +1,16 @@
+//Grasshopper - Check for factor
+
+/* INSTRUCTIONS
+Check for Factor
+This function should test if the factor is a factor of base.
+
+Return true if it is a factor or false if it is not. */
+
+function checkForFactor (base, factor) {
+  // code here
+  if (base % factor == 0){
+  return true
+  } else {
+  return false
+  }
+}
