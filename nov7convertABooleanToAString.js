d608263c8cbc09779ded6166928d7730c28b5431
Betances <tonybetancesdev@gmail.com>
2019-11-07 22:16:21 -0500

    CodeWars / Convert A Boolean To A String / JS / https://www.codewars.com/kata/551b4501ac0447318f0009cd

---
+++ nov7convertABooleanToAString.js
@@ -0,0 +1,14 @@
+/* Details
+In this programming exercise, you're going to learn about functions, boolean (true/false) values, strings, and the if-statement.
+
+function booleanToString(b){
+  //your code here
+}
+
+ANSWER:
+
+*/
+
+function booleanToString(b){
+return b.toString()
+}
