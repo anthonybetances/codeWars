3ce027dfa40adf643ec99dc6f36c1266ab96db28
Betances <tonybetancesdev@gmail.com>
2019-11-12 23:47:38 -0500

    CodeWars / Sentence to Words / JS / https://www.codewars.com/kata/57a05e0172292dd8510001f7

---
+++ nov12SentenceToWords.js
@@ -0,0 +1,10 @@
+//Sentence to Words
+
+//Instructions
+//Write function splitSentence which will create a list of strings from a string.
+
+//Answer:
+
+function splitSentence(s) {
+  return s.split(" ");
+}
